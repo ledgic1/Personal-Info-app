ea7a2c4d6932c6b7fda2c1f55a051333b2044f51
Brandon M <Brandon.maksin@yahoo.com>
2025-05-10 14:59:31 -0400

    Removed unecessary files from repository

--- myvariable.java
+++
@@ -1,7 +0,0 @@
-public class BasicReview {
-	
-	public static void main(String[] args) {
-		String avariable = "example value";
-		System.out.println("the value of avariable is " + avariable);
-	}
-}
